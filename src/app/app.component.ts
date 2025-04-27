5c979486ae02a1b67e8dcb8493835c911580477f
Nourhan <nourhanmahmed1@gmail.com>
2025-04-27 15:10:31 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as gplay from 'google-play-scraper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-store';
+  ngOnInit(): void {
+    // gplay
+    //   .app({ appId: 'com.google.android.apps.translate' })
+    //   .then(console.log, console.log);
+  }
 }
